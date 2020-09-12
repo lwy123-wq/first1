ff87ca53648713c3dddfa239fc726b8a88d95fbe
lwy123-wq <l2158223895@qq.com>
2020-09-12 21:34:44 +0800

    first

---
+++ src/aaa/Demo.java
@@ -0,0 +1,26 @@
+package aaa;
+
+import java.io.*;
+
+public class Demo{
+    public static void main(String[] args)
+    {
+        try{
+            FileInputStream fis = new FileInputStream(
+                    "FileOutputStreamTest.java");
+            FileOutputStream fos = new FileOutputStream("newFile.txt",true);
+            byte[] bbuf = new byte[32];
+            int hasRead = 0;
+            while ((hasRead = fis.read(bbuf)) > 0 )
+            {
+                fos.write(bbuf , 0 , hasRead);
+            }
+            fos.close();
+            fis.close();
+        }
+        catch (IOException ioe)
+        {
+            ioe.printStackTrace();
+        }
+    }
+}
